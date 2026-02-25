906d814ccfbbb44eb6780b3c46c6030d35dbb8d6
Raghav Bakshi <ragbak1210@gmail.com>
2026-02-25 13:58:08 +0530

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+import Navigation from '@/components/Navigation';
+import Footer from '@/components/Footer';
+import '@/globals.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Navigation />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
