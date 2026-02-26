45cf40b1f4364eeeb9f652a168c52c73d8a87009
Raghav Bakshi <ragbak1210@gmail.com>
2026-02-26 15:54:27 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,8 @@ export default function Home() {
 
 
 {/* ================= HERO ================= */}
-
-<section className="hero-premium bg-animated-gradient">      
-<section className="hero-premium bg-hero-premium relative">
+    
+<section className="hero-premium bg-animated-gradient bg-hero-premium relative">
 
 <div className="yellow-glow top-[-120px] left-[-120px]"></div>
 <div className="yellow-glow bottom-[-120px] right-[-120px]"></div>
